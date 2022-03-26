a929faf333045c1d434f444c1afcb0438451faa3
AnomolyAvery <ashawsolutions@gmail.com>
2022-03-26 13:39:35 -0400

    Added creating bread posts ability

--- views/index.jsx
+++ views/index.jsx
@@ -6,6 +6,11 @@ const Index = ({ breads }) => {
     return (
         <Default>
             <h2>Index Page.</h2>
+            <div className="newButton">
+                <a href="/breads/new">
+                    <button>Add a new bread</button>
+                </a>
+            </div>
             <ul>
                 {breads.map((bread, i) => (
                     <a href={`/breads/${i}`}>
---
+++ views/new.jsx
@@ -0,0 +1,36 @@
+const React = require('react');
+const Default = require('./layouts/default');
+
+const New = (props) => {
+    return (
+        <Default>
+            <h2>Add a new bread.</h2>
+            <div className="backButton">
+                <a href="/breads">
+                    <button>Go back to the index</button>
+                </a>
+            </div>
+            <form action="/breads" method="POST">
+                <label htmlFor="name">Name</label>
+                <input type="text" name="name" id="name" required />
+
+                <label htmlFor="image">Image</label>
+                <input type="text" name="image" id="image" />
+
+                <label htmlFor="gluten">Has Gluten?</label>
+                <input
+                    type="checkbox"
+                    name="gluten"
+                    id="gluten"
+                    defaultChecked
+                />
+
+                <br />
+
+                <input type="submit" />
+            </form>
+        </Default>
+    );
+};
+
+module.exports = New;
